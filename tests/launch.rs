3a3b3f3f63a211eaf53dc6622ba227101e91d6b3
Tyler Fanelli <tfanelli@redhat.com>
2025-04-11 23:48:34 -0400

    tests: Create nitro enclave VM
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- tests/launch.rs
+++ tests/launch.rs
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
-use nitro_enclaves::device::Device;
+use nitro_enclaves::{device::Device, launch::Launcher};
 
 #[test]
 fn launch() {
-    let _device = Device::open().unwrap();
+    let device = Device::open().unwrap();
+
+    let _launcher = Launcher::new(device);
 }
