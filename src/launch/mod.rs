5c7eff5387745da5d771a5db6123b095cee59ca2
Tyler Fanelli <tfanelli@redhat.com>
2025-04-17 22:06:32 -0400

    launch: Further document APIs
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- src/launch/mod.rs
+++ src/launch/mod.rs
@@ -14,7 +14,7 @@ use std::os::fd::{AsRawFd, RawFd};
 
 const VMADDR_CID_PARENT: u32 = 3;
 
-/// Facilitates the correct execution of the nitro enclaves launch process.
+/// Facilitates the execution of the nitro enclaves launch process.
 pub struct Launcher {
     vm_fd: RawFd,
     slot_uid: u64,
@@ -22,8 +22,7 @@ pub struct Launcher {
 }
 
 impl Launcher {
-    /// Begin the nitro enclaves launch process by creating a Launcher and issuing the NE_CREATE_VM
-    /// ioctl.
+    /// Begin the nitro enclaves launch process by creating a new enclave VM.
     pub fn new(dev: &Device) -> Result<Self, LaunchError> {
         let mut slot_uid: u64 = 0;
         let vm_fd = unsafe { libc::ioctl(dev.as_raw_fd(), NE_CREATE_VM as _, &mut slot_uid) };
@@ -44,12 +43,13 @@ impl Launcher {
         self.slot_uid
     }
 
-    /// Set enclave memory from an enclave image type and given size.
+    /// Allocate enclave memory and populate it with the enclave image.
     pub fn mem_set(&mut self, mem: MemoryInfo) -> Result<(), LaunchError> {
         // Load the VM's enclave image type and fetch the offset in enclave memory of where to
         // start placing the enclave image.
         let mut load_info = ImageLoadInfo::from(&mem.image_type);
 
+        // Get the image offset.
         let ret = unsafe {
             libc::ioctl(
                 self.vm_fd.as_raw_fd(),
@@ -62,11 +62,15 @@ impl Launcher {
             return Err(LaunchError::ioctl_err_from_errno());
         }
 
+        // Allocate the memory regions from the requested size.
         let mut regions = UserMemoryRegions::new(mem.size_mib).map_err(LaunchError::MemInit)?;
+
+        // Populate the memory regions with the contents of the enclave image.
         regions
             .image_fill(load_info.memory_offset as usize, mem.image_type)
             .map_err(LaunchError::MemInit)?;
 
+        // Add each memory region.
         for r in regions.inner_ref() {
             let ret = unsafe { libc::ioctl(self.vm_fd, NE_SET_USER_MEMORY_REGION, r) };
             if ret < 0 {
@@ -100,6 +104,8 @@ impl Launcher {
     pub fn start(&self, flags: StartFlags, cid: Option<u64>) -> Result<u64, LaunchError> {
         let mut cid = cid.unwrap_or(0);
 
+        // Ensure that a valid CID is used. If the current CID is invalid, randomly-generate a
+        // valid one.
         loop {
             if cid > VMADDR_CID_PARENT as u64 && cid <= u32::MAX as u64 {
                 break;
@@ -110,6 +116,7 @@ impl Launcher {
                 .map_err(|_| LaunchError::CidRandomGenerate)? as u64;
         }
 
+        // Start the enclave VM.
         let mut start_info = StartInfo::new(flags, cid);
 
         let ret = unsafe { libc::ioctl(self.vm_fd, NE_START_ENCLAVE as _, &mut start_info) };
